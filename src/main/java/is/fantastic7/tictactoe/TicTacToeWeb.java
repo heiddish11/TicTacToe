e06af4b6397223472c5048b0a33f55001b446645
GudrunInga <gudrunib09>
2014-11-01 17:09:00 +0000

    spark with heroku

---
+++ src/main/java/is/fantastic7/tictactoe/TicTacToeWeb.java
@@ -0,0 +1,43 @@
+package is.fantastic7.tictactoe;
+
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+
+public class TicTacToeWeb implements SparkApplication 
+{
+	public static void main(String[] args)
+	{
+		SparkApplication tictactoe = new TicTacToeWeb();
+		String port = System.getenv("PORT");
+		if (port != null)
+		{
+			setPort(Integer.valueOf(port));
+		}
+		tictactoe.init();
+	}
+
+	public void init()
+	{
+		final TicTacToe newGame = new TicTacToe();
+
+		post(new Route("/")
+		{
+			@Override
+			public Object handle(Request request, Response response)
+			{
+				String board = newGame.displayBoard();
+				return board;
+			}
+		});
+
+		/*post(new Route("/player")
+		{
+			@Override
+			public Object handle(Request request, Response response)
+			{
+
+			}
+		}*/
+	}
+}
